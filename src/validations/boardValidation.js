0bd17caab75ed2197d48aaaf670186dacc12368b
quyen6 <quyenthai.060603@gmail.com>
2025-08-07 20:03:29 +0700

    Error Handling

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from "joi";
 import { StatusCodes } from "http-status-codes";
+import ApiError from "~/utils/ApiError";
 
 const createNew = async (req, res, next) => {
   /**
@@ -25,10 +26,14 @@ const createNew = async (req, res, next) => {
     // Validate dữ liệu xong xuôi hợp lệ thì cho request đi tiếp sang Controller
     next();
   } catch (error) {
-    console.log(error);
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message,
-    });
+    // const errorMessage = new Error(error).message;
+    // const customError = new ApiError(
+    //   StatusCodes.UNPROCESSABLE_ENTITY,
+    //   errorMessage
+    // );
+    next(
+      new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message)
+    );
   }
 };
 
