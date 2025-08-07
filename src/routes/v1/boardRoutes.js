f815d84985f65d1667570662a8be0d44b7270db8
quyen6 <quyenthai.060603@gmail.com>
2025-08-07 11:32:07 +0700

    Express Router

---
+++ src/routes/v1/boardRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+const Router = express.Router();
+
+Router.route("/")
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({ message: "API v1 get list board " });
+  })
+  .post((req, res) => {
+    res.status(StatusCodes.CREATED).json({ message: "API v1 create " });
+  });
+
+export const boardRoutes = Router;
