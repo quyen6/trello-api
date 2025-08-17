578fdc0667bad94c56b5b3b2edbc931cb836b20b
quyen6 <quyenthai.060603@gmail.com>
2025-08-17 15:58:52 +0700

    Update CORS not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 // Những domain được phép truy cập tới tài nguyên của Server
-export const WHITELIST_DOMAINS = ["http://localhost:5173"];
+// export const WHITELIST_DOMAINS = ["http://localhost:5173"];
+// không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev
 
 export const BOARD_TYPE = {
   PUBLIC: "Public",
