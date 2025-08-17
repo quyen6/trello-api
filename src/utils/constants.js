f1a792cdfb5bfc1294ec809470b5f9834c9efd96
quyen6 <quyenthai.060603@gmail.com>
2025-08-17 17:27:45 +0700

    Update Deploy p2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
   // "http://localhost:5173"
-  "https://trellodnd.netlify.app/",
+  "https://trellodnd.netlify.app",
 ];
 // không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev
 
