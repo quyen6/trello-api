befb8d255224e56581b020839ecb9b514033f715
quyen6 <quyenthai.060603@gmail.com>
2025-08-24 16:02:10 +0700

    Mail Service & handle Register, Verify Account

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,10 +2,13 @@ import bcryptjs from "bcryptjs";
 import { StatusCodes } from "http-status-codes";
 import { v4 as uuidv4 } from "uuid";
 import { userModel } from "~/models/userModel";
+import { BrevoProvider } from "~/providers/BrevoProvider";
 import ApiError from "~/utils/ApiError";
+import { WEBSITE_DOMAIN } from "~/utils/constants";
 import { pickUser } from "~/utils/formatters";
 
 const createNew = async (reqBody) => {
+  // eslint-disable-next-line no-useless-catch
   try {
     // Kiểm tra xem email đã tồn tại trong hệ thống của chúng ta chưa
     const existUser = await userModel.findOneByEmail(reqBody.email);
@@ -27,12 +30,25 @@ const createNew = async (reqBody) => {
     // Thực hiện lưu thông tin vào database
     const createdUser = await userModel.createNew(newUser);
     const getNewUser = await userModel.findOneById(createdUser.insertedId);
+
     // Gửi Email cho người dùng xác thực tài khoản
+    const verificationLink = `${WEBSITE_DOMAIN}/account/verification?email=${getNewUser.email}&token=${getNewUser.verifyToken}`;
+    const customSubject =
+      "Trello Project: Please verify your email before using our services!";
+    const htmlContent = `
+      <h3>Here is your verification link: </h3>
+      <h3>${verificationLink}</h3>
+      <h3>Sincerely, <br /> - Tira Thai - </h3>
+      `;
+    // Gọi tới Provider gửi mail
+    await BrevoProvider.sendEmail(getNewUser.email, customSubject, htmlContent);
 
     // Retrun trả về dữ liệu cho phía Contronller
+    // pickUser: Lấy một vài dữ liệu cụ thể trong user để tránh việc trả về các dữ liệu nhạy cảm như hash password, verifyToken
     return pickUser(getNewUser);
   } catch (error) {
-    throw Error(error);
+    console.error("❌ Error:", error.response?.body || error);
+    throw error;
   }
 };
 
