ffb9d4d7d13b6d31a2cc062f48b7149da805e16a
quyen6 <quyenthai.060603@gmail.com>
2025-08-26 12:05:40 +0700

    Complete verify account, login, Jwt Token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,7 +11,32 @@ const createNew = async (req, res, next) => {
     next(error);
   }
 };
+const verifyAccount = async (req, res, next) => {
+  try {
+    // Điều hướng dữ liệu sang tầng Service
+    const result = await userService.verifyAccount(req.body);
+
+    // Có kết quả thì trả về Client
+    res.status(StatusCodes.OK).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+const login = async (req, res, next) => {
+  try {
+    // Điều hướng dữ liệu sang tầng Service
+    const result = await userService.login(req.body);
+
+    // Xử lý về http only cookie cho phía trình duyệt
+    // console.log("🚀 ~ login ~ result:", result);
+    res.status(StatusCodes.OK).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const userController = {
   createNew,
+  verifyAccount,
+  login,
 };
